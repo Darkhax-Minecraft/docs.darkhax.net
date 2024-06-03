d3af81e5ba24ebbbe24b1a697627692bbf3acc66
Tyler Hancock <darklime@live.ca>
2024-06-02 23:58:05 -0600

    Allow the cf_widget helper to use used outside of remark plugins.

--- src/lib/markdown/cf_widget.ts
+++ src/lib/markdown/cf_widget.ts
@@ -4,24 +4,36 @@ import type {Node} from "mdast";
 
 const projectCache = new Map<string, Promise<CurseWidgetData>>();
 
-export async function getProjectData(projectId: string, file: VFile, node: Node): Promise<CurseWidgetData> {
-    if (!projectCache.has(projectId)) {
+export async function getProjectData(projectId: string | number, file: VFile | undefined = undefined, node: Node | undefined = undefined): Promise<CurseWidgetData> {
+    const id = projectId.toString();
+    if (!projectCache.has(id)) {
         projectCache.set(
-            projectId,
+            id,
             fetch(`https://api.cfwidget.com/${projectId}`)
                 .then<CurseWidgetData>((res) => res.json())
                 .then((value) => {
                     console.log(`Caching project ${projectId}`);
                     return value;
                 })
-                .catch((reason) => file.fail(`Could not get project data. reason=${reason}`, node))
+                .catch((reason) => fail(`Could not get project data. reason=${reason}`, file, node))
         );
     }
-    const ret = projectCache.get(projectId);
+    const ret = projectCache.get(id);
     if (!ret) {
-        file.fail(`Could not get project data. reason=${projectId}`);
+        fail(`Could not get project data. reason=${projectId}`);
     }
     else {
         return ret;
+    }
+}
+
+function fail(reason: string, file: VFile | undefined = undefined, node: Node | undefined = undefined): never {
+
+    if (file && node) {
+        return file.fail(reason, node)
+    }
+    else {
+        console.error(reason)
+        throw new Error(reason)
     }
 }
