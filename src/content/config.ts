94984fde70fd7aa8524b615c6481f211b78dceb3
Tyler Hancock <darklime@live.ca>
2024-06-02 23:56:59 -0600

    Fix game version in the config.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
-export const gameVersion = '1.20.4'
+export const gameVersion = '1.20.1'
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
 };
