31ed0a3ef3a93ad356178f48a56727b94d526998
Tyler Hancock <darklime@live.ca>
2024-06-04 04:53:37 -0600

    Re-export the starlight components.

--- src/components/Components.ts
+++ src/components/Components.ts
@@ -1,2 +1,8 @@
+// Vanilla Starlight
+export * from '@astrojs/starlight/components'
+
+// Custom Components
+// @ts-ignore
 export { default as ModCard } from './ModCard.astro';
+// @ts-ignore
 export { default as Link } from './Link.astro';
